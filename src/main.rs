2b19c7fea3704541048b7884f94ed7481f486e49
hikari <me@metantesan.com>
2025-10-29 17:51:34 +0330

    feat: Add ASCII art logo to CLI startup

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use eyre::{Error, Result};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    const LOGO: &str = include_str!("director.ascii");
+    println!();
+    for line in LOGO.lines() {
+        println!(" {line}");
+    }
+    println!("");
     color_eyre::install()?;
     tracing_subscriber::fmt::init();
 
